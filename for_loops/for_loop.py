2e0b6986e9edef539459a10f16d9f9d9f8f649fa
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-25 08:48:42 +0100

    Add files via upload

---
+++ for_loops/for_loop.py
@@ -0,0 +1,14 @@
+# used to iterate items of a collection such as a string
+# we can deine lists using square brakets
+# for item in ['Elijah', 'Tochi', 'Eze']:
+#     print(item)
+    # we use the range funtion to type multiple numbers
+# for item in range(55,100,3):
+#     print(item)
+  # adding steps at the last number in the range bracket
+prices = [10,20,30]
+
+total = 0
+for price in prices:
+    total += price
+    print("total: {price}")
